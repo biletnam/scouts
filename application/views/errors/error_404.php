c605d0898641c2760af296d6df15cf8e9ef5007b
Tim van Dijck <tim.van.dijck@hotmail.com>
2018-03-14 22:53:41 +0100

    remove CI code

--- application/views/errors/error_404.php
+++
@@ -1,7 +0,0 @@
-<main>
-	<h1>Oeps! oeps niet gevonden!</h1>
-	<p>
-		De webpagina die u probeerde te bereiken is helaas niet beschikbaar. 
-		Klik <a href="http://18bp.be">hier</a> om terug te keren naar de startpagina.
-	</p>
-</main>
